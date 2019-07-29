53d7599607156368e48f134da0a2e32b63107bd7
Wenqi Jiang <wj2285@columbia.edu>
2019-07-29 08:46:16 -0400

    download 6144 h5

--- h5/split_h5.sh
+++ h5/split_h5.sh
@@ -1,39 +1,39 @@
 # embedding layer
-python splitH5.py --iname="./train-embeddings-rnn-50-8192.h5" \
-				--dataset="/model_weights/embedding_1/embedding_1/embeddings:0" \
-				--oname="./embedding_1_embeddings.h5" \
-				--new_dataset="/embedding_1/embeddings:0"
+python splitH5.py --iname="./train-embeddings-rnn-50-6144.h5" \
+				--dataset="/model_weights/embedding_3/embedding_3/embeddings:0" \
+				--oname="./embedding_3_embeddings.h5" \
+				--new_dataset="/embedding_3/embeddings:0"
 				# h5dump -d "/embedding_1/embeddings:0" embedding_1_embeddings.h5
 
 # RNN layer
-python splitH5.py --iname="./train-embeddings-rnn-50-8192.h5" \
-				--dataset="/model_weights/simple_rnn_1/simple_rnn_1/bias:0" \
-				--oname="./simple_rnn_1_bias.h5" \
-				--new_dataset="/simple_rnn_1/bias:0"
+python splitH5.py --iname="./train-embeddings-rnn-50-6144.h5" \
+				--dataset="/model_weights/simple_rnn_3/simple_rnn_3/bias:0" \
+				--oname="./simple_rnn_3_bias.h5" \
+				--new_dataset="/simple_rnn_3/bias:0"
 				# h5dump -d "/simple_rnn_1/bias:0" simple_rnn_1_bias.h5
 
-python splitH5.py --iname="./train-embeddings-rnn-50-8192.h5" \
-				--dataset="/model_weights/simple_rnn_1/simple_rnn_1/kernel:0" \
-				--oname="./simple_rnn_1_kernel.h5" \
-				--new_dataset="/simple_rnn_1/kernel:0"
+python splitH5.py --iname="./train-embeddings-rnn-50-6144.h5" \
+				--dataset="/model_weights/simple_rnn_3/simple_rnn_3/kernel:0" \
+				--oname="./simple_rnn_3_kernel.h5" \
+				--new_dataset="/simple_rnn_3/kernel:0"
 				# h5dump -d "/simple_rnn_1/kernel:0" simple_rnn_1_kernel.h5
 
-python splitH5.py --iname="./train-embeddings-rnn-50-8192.h5" \
-				--dataset="/model_weights/simple_rnn_1/simple_rnn_1/recurrent_kernel:0" \
-				--oname="./simple_rnn_1_recurrent_kernel.h5" \
-				--new_dataset="/simple_rnn_1/recurrent_kernel:0"
+python splitH5.py --iname="./train-embeddings-rnn-50-6144.h5" \
+				--dataset="/model_weights/simple_rnn_3/simple_rnn_3/recurrent_kernel:0" \
+				--oname="./simple_rnn_3_recurrent_kernel.h5" \
+				--new_dataset="/simple_rnn_3/recurrent_kernel:0"
 				# h5dump -d "/simple_rnn_1/recurrent_kernel:0" simple_rnn_1_recurrent_kernel.h5
 
 # dense layer
 
-python splitH5.py --iname="./train-embeddings-rnn-50-8192.h5" \
-				--dataset="/model_weights/dense_1/dense_1/bias:0" \
-				--oname="./dense_1_bias.h5" \
-				--new_dataset="/dense_1/bias:0"
+python splitH5.py --iname="./train-embeddings-rnn-50-6144.h5" \
+				--dataset="/model_weights/dense_3/dense_3/bias:0" \
+				--oname="./dense_3_bias.h5" \
+				--new_dataset="/dense_3/bias:0"
 				# h5dump -d "/dense_1/bias:0" dense_1_bias.h5
 
-python splitH5.py --iname="./train-embeddings-rnn-50-8192.h5" \
-				--dataset="/model_weights/dense_1/dense_1/kernel:0" \
-				--oname="./dense_1_kernel.h5" \
-				--new_dataset="/dense_1/kernel:0"
+python splitH5.py --iname="./train-embeddings-rnn-50-6144.h5" \
+				--dataset="/model_weights/dense_3/dense_3/kernel:0" \
+				--oname="./dense_3_kernel.h5" \
+				--new_dataset="/dense_3/kernel:0"
 				# h5dump -d "/dense_1/kernel:0" dense_1_kernel.h5
